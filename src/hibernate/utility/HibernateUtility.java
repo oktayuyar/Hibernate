5e53117161ee0dee0725764cebff3341f1cc1c02
Oktay UYAR <oktyuyar@gmail.com>
2017-02-13 15:17:32 +0200

    updates

--- src/hibernate/utility/HibernateUtility.java
+++ src/hibernate/utility/HibernateUtility.java
@@ -5,13 +5,17 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtility {
-	public static Session getHibernateSession() {
-
-	    final SessionFactory sf = new Configuration()
+	
+	private final static SessionFactory sf = new Configuration()
 	        .configure("hibernate.cfg.xml").buildSessionFactory();
-
-	    final Session session = sf.openSession();
-	    return session;
-	    }
-
+    	private volatile static Session session = sf.openSession();
+    
+	private HibernateUtility() {
+	}
+	public static Session getHibernateSession() {
+	      if(session == null) {
+	    	  session = (Session) new HibernateUtility();
+	       }
+	       return session;
+	}
 }
